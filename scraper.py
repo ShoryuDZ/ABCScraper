e962d4cc5bf45beedb729ff3d30b4b19713578bc
Shoryu Das-Zaman <58425525+ShoryuDZ@users.noreply.github.com>
2020-01-27 23:49:47 +1100

    Monthly Outputs for Scraper
    As there are request limits for the ABC Archive, this program allows users to terminate the program and restart, with output being saved every month.

--- scraper.py
+++ scraper.py
@@ -1,12 +1,7 @@
 import requests
 import urllib.request
-import time
 from bs4 import BeautifulSoup
 from docx import Document
-
-#Opens .docx file and initialises list of articles which cannot be added
-document = Document()
-failedArticles = []
 
 #Performs main function of parsing HTML, 1 run per day
 def mainParser():
@@ -60,17 +55,25 @@ def mainParser():
 
         counter += 1
 
-#Getting all articles between 1/1/2009 & 31/12/2019
-year = 2009
-targetYear = 2019
-month = 1
+#Getting days which scraper should collect 
+startingInput = input("Enter starting date (mm/yyyy): ").split('/', 2)
+targetYear = int(input("Enter ending year (yyyy): "))
+
+year = int(startingInput[1])
+month = int(startingInput[0])
 day = 1
 
+#Ensuring number of days in each month is correct
 monthsOf31 = [1, 3, 5, 7, 8, 10, 12]
 monthsOf30 = [4, 6, 9, 11]
 
 while year <= targetYear:
     while month <= 12:
+        
+        #Opens .docx file and initialises list of articles which cannot be added
+        document = Document()
+        failedArticles = []
+        
         if month in monthsOf31:
             daysInMonth = 31
         elif month in monthsOf30:
@@ -80,21 +83,23 @@ while year <= targetYear:
                 daysInMonth = 29
             else:
                 daysInMonth = 28
-
+        
+        #Looping for the number of days within the month
         while day <= daysInMonth:
             print(str(day) + '/' + str(month) + '/' + str(year))
             mainParser()
             day += 1
-
+        
+        #Adding failed articles and saving the document
+        failedArticlesParagraph = document.add_paragraph()
+        for failedArticle in failedArticles:
+            newLine = document.add_run(failedArticle)
+            newLine.add_break()
+        
+        document.save('{0}/{1} results.docx'.format(month, year))
+        
         month += 1
         day = 1
+        
     year += 1
-    month = 1
-
-#Adding failed articles and saving the document
-failedArticlesParagraph = document.add_paragraph()
-for failedArticle in failedArticles:
-    newLine = document.add_run(failedArticle)
-    newLine.add_break()
-document.save('results.docx')
-    
+    month = 1    
